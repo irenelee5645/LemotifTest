f5508153b11fd862382f7f009dbad9ae8d5c88c4
irenelee5645 <irenecharmeee@gmail.com>
2020-06-07 23:15:48 +0900

    first commit

---
+++ Lemotif_test/SecondViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecondViewController.swift
+//  Lemotif_test
+//
+//  Created by Irene Lee on 5/31/20.
+//  Copyright © 2020 Irene Lee. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+@IBOutlet  weak var view1: UIImageView!
+    
+    @IBOutlet weak var view2: UIImageView!
+    
+    @IBOutlet weak var view3: UIImageView!
+    
+     override func viewDidLoad() {
+       
+    }
+
+}
